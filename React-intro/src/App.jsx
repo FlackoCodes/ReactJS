e4efc5538ed6863abf25212133ad94fad1704e2b
Flacko <lesliejoe2k2@gmail.com>
2024-06-02 12:55:00 +0000

    my first component

--- React-intro/src/App.jsx
+++ React-intro/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-
+import Header from './header'
 
 
 function App() {
@@ -10,6 +10,7 @@ function App() {
 
   return (
     <>
+    <Header />
       <div>
         <a href="https://vitejs.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
@@ -30,9 +31,11 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <p>My First JSX line</p>
     </>
   )
 }
 
 export default App
+
 
---
+++ React-intro/src/header.jsx
@@ -0,0 +1,19 @@
+function Header() {
+    return (
+        <div>
+            <nav>
+                <ul>
+                    <li>React</li>
+                    <li>Tuts</li>
+                    <li>Vue</li>
+                    <li>Angular</li>
+                    <li>Typescript</li>
+                </ul>
+            </nav>
+
+        </div>
+        
+    )
+}
+
+export default Header
