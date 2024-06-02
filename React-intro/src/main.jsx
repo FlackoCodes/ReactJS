74373b6c8a88cc46f626a1948effac89dd8039ee
Flacko <lesliejoe2k2@gmail.com>
2024-06-02 13:29:18 +0000

    modified files

--- React-intro/src/main.jsx
+++ React-intro/src/main.jsx
@@ -3,6 +3,10 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 
+
+// App is the main component, all other components created will go into the App file
+
+// code below injects all our jsx in the root element(div) in the html
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
