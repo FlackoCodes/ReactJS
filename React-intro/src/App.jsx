3718f040ad6031a61fde200f6799410856557e91
Flacko <lesliejoe2k2@gmail.com>
2024-06-02 19:50:34 +0000

    new feat: add delete button using react-icons

--- React-intro/src/App.jsx
+++ React-intro/src/App.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import './index.css'
 import Header from './components/Header';
 import Tasks from './components/Tasks';
@@ -5,11 +6,30 @@ import Tasks from './components/Tasks';
 
 function App() {
  
-
+  const  [ tasks, setTasks ] = useState( [
+    {
+        id: 1,
+        text: 'Continue writing React',
+        day: 'Monday, All Day',
+        reminder: true
+    },
+    {
+        id: 2, // Updated id to be unique
+        text: 'Revise Asynchronous and Promises',
+        day: 'Tuesday Before Work',
+        reminder: true
+    },
+    {
+        id: 3,
+        text: 'Skip Work',
+        day: 'Wednesday Morning',
+        reminder: false
+    }
+]);
   return (
    <div className='container'>
      <Header />
-     <Tasks />
+     <Tasks tasks={tasks} />
    </div>
   
   )
--- React-intro/src/components/Tasks.jsx
+++ React-intro/src/components/Tasks.jsx
@@ -1,34 +1,19 @@
-import { useState } from "react";
+import PropTypes from 'prop-types';
+import Task from './Task';
 
+const Tasks = ({ tasks }) => {
 
-const Tasks = () => {
-  const  [ tasks, setTasks ] = useState( [
-        {
-            id: 1,
-            text: 'Continue writing React',
-            day: 'Monday',
-            reminder: true
-        },
-        {
-            id: 2, // Updated id to be unique
-            text: 'Revise Asynchronous and Promises',
-            day: 'Tuesday',
-            reminder: true
-        },
-        {
-            id: 3,
-            text: 'Skip Work',
-            day: 'Wednesday',
-            reminder: false
-        }
-    ]);
   return (
     <>
       {tasks.map(task => (
-        <h2 key={task.id}>{task.text}</h2>
+        <Task key={task.id} task={task} />
       ))}
     </>
   );
 };
 
+
+Tasks.PropTypes = {
+    tasks: PropTypes.string
+}
 export default Tasks;
