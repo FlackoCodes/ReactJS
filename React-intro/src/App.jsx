f910ca38548e7f42dac9e61a36ef50202ffacf18
Flacko <lesliejoe2k2@gmail.com>
2024-06-03 21:28:25 +0000

    rebuilt app

--- React-intro/src/App.jsx
+++ React-intro/src/App.jsx
@@ -1,46 +1,16 @@
-import { useState } from "react";
 import './index.css'
-import Header from './components/Header';
-import Tasks from './components/Tasks';
 
 
-function App() {
- 
-  const  [ tasks, setTasks ] = useState( [
-    {
-        id: 1,
-        text: 'Continue writing React',
-        day: 'Monday, All Day',
-        reminder: true
-    },
-    {
-        id: 2, // Updated id to be unique
-        text: 'Revise Asynchronous and Promises',
-        day: 'Tuesday Before Work',
-        reminder: true
-    },
-    {
-        id: 3,
-        text: 'Skip Work',
-        day: 'Wednesday Morning',
-        reminder: false
-    }
-]);
 
+const App = () => {
 
-function deleteTask(id) {
-  console.log('delete', id);
-}
+  const monNom = 'Flacko'
 
   return (
-   <div className='container'>
-     <Header />
-     <Tasks tasks={tasks}  onDelete ={deleteTask}/>
-   </div>
-  
+    <div>
+      {monNom} is back studying React
+    </div>
   )
 }
 
 export default App
-
-
