b0e20b3535971446d1619bdfd691d8ebf9283261
Flacko <lesliejoe2k2@gmail.com>
2024-06-05 05:35:57 +0000

    created links component

--- social-links/social-links/src/App.jsx
+++ social-links/social-links/src/App.jsx
@@ -1,35 +1,31 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import avatar from './assets/images/avatar-jessica.jpeg'
+import SocialLinks from './SocialLinks'
+import './index.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+     <div className="container">
+      <div className="hero">
+        <div>
+          <img src={avatar} alt="avatar" className='avatar' style={style}/>
+          <h2 className="name">Jessica Randall</h2>
+          <p className="color">London, United Kingdom</p>
+        </div>
+        <p className="skill">&quot;Front-end developer and avid reader.&quot;</p>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <SocialLinks />
+     </div>
     </>
   )
+}
+
+const style = {
+  width: '80px',
+  height: '80px',
+  borderRadius: '50%',
+  margin: '5px 0'
 }
 
 export default App
